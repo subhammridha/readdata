b5f2661b1e3470b44406e0b2a2ee3d677649a566
subhammridha <subhammridha@users.noreply.github.com>
2017-04-06 17:03:43 +0530

    modified readdata file

--- readdata.py
+++ readdata.py
@@ -1,5 +1,5 @@
 # Code to read lammps data file.
-structpath = '/home/subham/bicrystal_structures/'
+structpath = '../../09-bicrystal_structures/'
 filepath = structpath+'data.albicrystal.s5.102.010.0'
 f = open(filepath,'r')
 for line in f:
